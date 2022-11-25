f4e838e33a95258aedec08e220fc117672bbee3f
Hyesunny12 <51188151+Hyesunny12@users.noreply.github.com>
2022-11-25 13:09:37 +0900

    the 1st coding
    hi!

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup(){
+    createCanvas(640, 480);
+    background(0, 255, 0); //RGB
+}
+//      point(40, -60);
+//      point(10, 200);
+//      point(200, 110);
+// }
+   //   function draw(){
+   //      point(mouseX, mouseY)
+   //   }
+    //  point(70, 30);
+function draw(){
+    line(10, 30, 90, 30);
+    line(10, 30, 100, 100);
+    line(90, 30, 10, 100);
+    line(10, 100, 50, 10);
+    line(50, 10, 100, 100);
+}
+
