c01428cf4fcdd4fd89968d89e94e7e97403a4734
Hyesunny12 <51188151+Hyesunny12@users.noreply.github.com>
2022-11-25 13:42:27 +0900

    add ellipse

--- sketch.js
+++ sketch.js
@@ -1,20 +1,18 @@
+
+let mySound;
+
 function setup(){
-    createCanvas(640, 480);
-    background(0, 255, 0); //RGB
-}
-//      point(40, -60);
-//      point(10, 200);
-//      point(200, 110);
-// }
-   //   function draw(){
-   //      point(mouseX, mouseY)
-   //   }
-    //  point(70, 30);
-function draw(){
-    line(10, 30, 90, 30);
-    line(10, 30, 100, 100);
-    line(90, 30, 10, 100);
-    line(10, 100, 50, 10);
-    line(50, 10, 100, 100);
+
+ soundFormats('mp3');
+ mySound = loadSound('말리지 마.mp3' , loaded);
+//  mySound.play();
 }
 
+function loaded(){
+    mySound.play();
+}
+
+function draw(){
+    fill(255);
+    ellipse(50,50,50,50);
+}
