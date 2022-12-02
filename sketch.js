1342d1676d8fbf753e34f486063a90ef0cc331c3
Hyesunny12 <51188151+Hyesunny12@users.noreply.github.com>
2022-12-02 14:33:44 +0900

    12.02 version

--- sketch.js
+++ sketch.js
@@ -1,27 +1,47 @@
 
 let song;
-let button;
+var button,jumpButton,reverseButton,rateButton
+var t;
+var len;
+var amp;
+var rb;
+var p = 0;
 
-function setup(){
-createCanvas(640,480);
-//  soundFormats('mp3','ogg');
-song = loadSound('1.mp3' , loaded);
-//  mySound.play();
-button = createButton("play");
+function setup() {
+createCanvas(200, 200);
+
+
+song = loadSound('No celestial.mp3', loaded);
+// song2 = loadSound('말리지 마.mp3', loaded);
+
+
+button = createButton('play');
 button.mousePressed(togglePlaying);
+
+jumpButton = createButton('jump');
+jumpButton.mousePressed(jumpSong);
+
+reverseButton = createButton('reverse');
+reverseButton.mousePressed(reverseSong);
+
+rateButton = createButton('X2');
+rateButton.mousePressed(rateSong);
+
+
+amp = new p5.Amplitude();
 
 }
 
 
-function togglePlaying(){
-   if(!song.isPlaying()){
-    song.play();
-    song.setVolume(0.3);
-    button.html('pause');
-   }else{
-    song.stop();
-    button.html('play');
-   }
+function togglePlaying() {
+if (!song.isPlaying()) {
+song.play();
+song.setVolume(0.3);
+button.html('pause');
+} else {
+song.stop();
+button.html('play');
+}
 }
 
 function loaded(){
@@ -29,6 +49,32 @@ function loaded(){
 }
 
 function draw(){
-    fill(255);
-    ellipse(60,60,40,40);
+    background(51);//이걸 안하면 흔적 남음
+    // fill(255);
+    // ellipse(60,60,40,40);
+    console.log(song.currentTime());  //현재시간 확인
+
+    /////////get level로 도형 컨트롤
+    var vol = amp.getLevel();
+    var diam = map(vol, 0, 0.3, 200, 0);
+    fill(255, 0, 255);
+    // ellipse(width / 2, height / 2, diam, diam);
+    ellipse(100, diam, 10, 10);
+  
 }
+
+function jumpSong() {
+len = song.currentTime();//현재시간
+t = len+10; //현재시간에서 10초씩 더하기
+song.jump(t);
+} 
+
+function reverseSong(){
+
+rb = song.reverseBuffer();
+}
+
+function rateSong(){
+    song.rate(2);
+}
+
